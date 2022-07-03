1a445468b50e21a1ce3e3e53fe3c3c7ecaa5a04f
METC <metc_ck1@hotmail.com>
2022-07-03 10:00:17 -0700

    17.Unittests for the console v4

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,8 +6,8 @@ Unittests for the Console.py file
 import cmd
 import pycodestyle
 import unittest
-
 import console
+HBNB = console.HBNBCommand
 
 
 class TestConsole(unittest.TestCase):
@@ -19,7 +19,7 @@ class TestConsole(unittest.TestCase):
         Test if test_console.py passes the pycodestyle style
         """
         style = pycodestyle.StyleGuide()
-        console_style_t = style.check_files(['test_console.py'])
+        console_style_t = style.check_files(['tests/test_console.py'])
         self.assertEqual(console_style_t.total_errors, 0, "Does not com\
         ply with pycodestyle")
 
@@ -38,56 +38,74 @@ class TestConsole(unittest.TestCase):
         """
         self.assertIsNotNone(console.__doc__, "Console doc documentat\
         ion does not exists")
-        self.assertIsNotNone(console.do_quit.__doc__, "Console do_quit\
+        self.assertIsNotNone(HBNB.do_quit.__doc__, "Console do_quit\
         Documentation does not exists")
-        self.assertIsNotNone(console.do_EOF.__doc__, "Console do_EOF\
+        self.assertIsNotNone(HBNB.do_EOF.__doc__, "Console do_EOF\
         Documentation does not exists")
-        self.assertIsNotNone(console.emptyline.__doc__, "Console\
+        self.assertIsNotNone(HBNB.emptyline.__doc__, "Console\
         do_emptyline Documentation does not exists")
-        self.assertIsNotNone(console.do_create.__doc__, "Console\
+        self.assertIsNotNone(HBNB.do_create.__doc__, "Console\
         do_create Documentation does not exists")
-        self.assertIsNotNone(console.do_show.__doc__, "Console do_show\
+        self.assertIsNotNone(HBNB.do_show.__doc__, "Console do_show\
         Documentation does not exists")
-        self.assertIsNotNone(console.do_destroy.__doc__, "Console\
+        self.assertIsNotNone(HBNB.do_destroy.__doc__, "Console\
         do_destroy Documentation does not exists")
-        self.assertIsNotNone(console.do_all.__doc__, "Console do_all\
+        self.assertIsNotNone(HBNB.do_all.__doc__, "Console do_all\
         Documentation does not exists")
-        self.assertIsNotNone(console.do_update.__doc__, "Console\
+        self.assertIsNotNone(HBNB.do_update.__doc__, "Console\
         do_update Documentation does not exists")
-        self.assertIsNotNone(console.default.__doc__, "Console default\
+        self.assertIsNotNone(HBNB.default.__doc__, "Console default\
         Documentation does not exists")
 
     def test_methods_doc_length(self):
         """
-        Test if the methods documentation in console.py have more than 5 words
+        Test if the methods documentation in console.py have mo\
+        re than 10 letters
         """
         n_con = len(console.__doc__)
         self.assertGreaterEqual(n_con, 10, "Console doc document\
-        ation has less than 5 words")
-        n_con_quit = len(console.do_quit.__doc__)
+        ation has less than 10 letters")
+        n_con_quit = len(HBNB.do_quit.__doc__)
         self.assertGreaterEqual(n_con_quit, 10, "Console do_quit Document\
-        ation has less than 5 words")
-        n_con_eof = len(console.do_EOF.__doc__)
+        ation has less than 10 letters")
+        n_con_eof = len(HBNB.do_EOF.__doc__)
         self.assertGreaterEqual(n_con_eof, 10, "Console do_EOF Documentat\
-        ion has less than 5 words")
-        n_con_emptyline = len(console.emptyline.__doc__)
+        ion has less than 10 letters")
+        n_con_emptyline = len(HBNB.emptyline.__doc__)
         self.assertGreaterEqual(n_con_emptyline, 10, "Console do_emptyline D\
-        ocumentation has less than 5 words")
-        n_con_create = len(console.do_create.__doc__)
+        ocumentation has less than 10 letters")
+        n_con_create = len(HBNB.do_create.__doc__)
         self.assertGreaterEqual(n_con_create, 10, "Console do_create Document\
-        ation has less than 5 words")
-        n_con_show = len(console.do_show.__doc__)
+        ation has less than 10 letters")
+        n_con_show = len(HBNB.do_show.__doc__)
         self.assertGreaterEqual(n_con_show, 10, "Console do_show Document\
-        ation has less than 5 words")
-        n_con_destroy = len(console.do_destroy.__doc__)
+        ation has less than 10 letters")
+        n_con_destroy = len(HBNB.do_destroy.__doc__)
         self.assertGreaterEqual(n_con_destroy, 10, "Console do_destroy Doc\
-        umentation has less than 5 words")
-        n_con_all = len(console.do_all.__doc__)
+        umentation has less than 10 letters")
+        n_con_all = len(HBNB.do_all.__doc__)
         self.assertGreaterEqual(n_con_all, 10, "Console do_all Document\
-        ation has less than 5 words")
-        n_con_update = len(console.do_update.__doc__)
+        ation has less than 10 letters")
+        n_con_update = len(HBNB.do_update.__doc__)
         self.assertGreaterEqual(n_con_update, 10, "Console do_update Document\
-        ation has less than 5 words")
-        n_con_default = len(console.default.__doc__)
+        ation has less than 10 letters")
+        n_con_default = len(HBNB.default.__doc__)
         self.assertGreaterEqual(n_con_default, 10, "Console default Document\
-        ation has less than 5 words")
+        ation has less than 10 letters")
+
+    def test_HBNBCommand_class_doc_exists(self):
+        """
+        Test if the console HBNBCommand class in console.py have document\
+        ation (test only if it exist)
+        """
+        self.assertIsNotNone(HBNB.__doc__, "Console HBNBCommand d\
+        ocumentation does not exists")
+
+    def test_HBNBCommand_doc_length(self):
+        """
+        Test if the HBNBCommand documentation in console.py has mo\
+        re than 10 letters
+        """
+        n_con = len(HBNB.__doc__)
+        self.assertGreaterEqual(n_con, 10, "Console HBNBCommand document\
+        ation has less than 10 letters")
